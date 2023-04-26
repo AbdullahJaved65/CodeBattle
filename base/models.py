92b496e56d0f459ff5728be6e6ebcc09cdd7428e
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-04-26 23:20:46 +0500

    - Added new users. - Fixed some errors in the models.

--- base/models.py
+++ base/models.py
@@ -6,13 +6,15 @@ class User(AbstractUser):
     name = models.CharField(max_length=255, null=True)
     email = models.EmailField(unique=True, null= True)
     bio = models.TextField(null=True, blank=True)
+    hackathon_participant = models.BooleanField(null= True, 
+                                                default=True)
     # avatar
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
 
-class Events(models.Model):
+class Event(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=500, null=True, blank=True)
     participants = models.ManyToManyField(User, blank=True)
@@ -25,9 +27,14 @@ class Events(models.Model):
     
 
 class Submission(models.Model):
-    participant = models.ForeignKey(User, on_delete= models.SET_NULL, null= True)
-    events = models.ForeignKey(Events, on_delete= models.SET_NULL, null= True)
-    details = models.TextField(max_length=500, null=True, blank=True)
+    participant = models.ForeignKey(User, 
+                                    on_delete= models.SET_NULL, 
+                                    null= True)
+    events = models.ForeignKey(Event, 
+                               on_delete= models.SET_NULL, 
+                               null= True)
+    details = models.TextField(max_length=500, 
+                               null=True, blank=True)
     
     def __str__(self) -> str:
         return str(self.events) + " --- " + str(self.participant)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import User, Event, Submission
 
 
 def home_page(request):
+    users = User.object.filter(hackathon_participant= True)
+    
     return render(request, 'home.html')
