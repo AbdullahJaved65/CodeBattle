23efec424b06c570813e46c02ec4c5011477e5db
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-05-02 17:02:44 +0500

    - Create 2 new pages - Added new models - Added content to Homepage (still bare bones)

--- base/models.py
+++ base/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=255, null=True)
-    email = models.EmailField(unique=True, null= True)
+    email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
-    hackathon_participant = models.BooleanField(null= True, 
+    hackathon_participant = models.BooleanField(null=True,
                                                 default=True)
     # avatar
-    
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
+
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
@@ -21,20 +21,20 @@ class Event(models.Model):
     date = models.DateTimeField()
     upadated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self) -> str:
         return self.name
-    
+
 
 class Submission(models.Model):
-    participant = models.ForeignKey(User, 
-                                    on_delete= models.SET_NULL, 
-                                    null= True)
-    events = models.ForeignKey(Event, 
-                               on_delete= models.SET_NULL, 
-                               null= True)
-    details = models.TextField(max_length=500, 
+    participant = models.ForeignKey(User,
+                                    on_delete=models.SET_NULL,
+                                    null=True)
+    events = models.ForeignKey(Event,
+                               on_delete=models.SET_NULL,
+                               null=True)
+    details = models.TextField(max_length=500,
                                null=True, blank=True)
-    
+
     def __str__(self) -> str:
         return str(self.events) + " --- " + str(self.participant)
--- base/views.py
+++ base/views.py
@@ -3,7 +3,15 @@ from .models import User, Event, Submission
 
 
 def home_page(request):
-    users = User.objects.filter(hackathon_participant= True)
+    users = User.objects.filter(hackathon_participant=True)
     events = Event.objects.all()
-    context = {'users':users, 'events':events}
+    context = {'users': users, 'events': events}
     return render(request, 'home.html', context)
+
+
+def event_page(request, pk):
+    event = Event.objects.get(id=pk)
+    context = {'event': event}
+    return render(request, 'event.html', context)
+
+# def registration_confirmation()
