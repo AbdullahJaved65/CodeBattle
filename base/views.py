c878b22357e0ae1f88a671e15b7d5fe58f014d07
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-05-02 15:55:14 +0500

    - Edited Homepage. - Added some users.

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from .models import User, Event, Submission
 
 
 def home_page(request):
-    users = User.object.filter(hackathon_participant= True)
-    
-    return render(request, 'home.html')
+    users = User.objects.filter(hackathon_participant= True)
+    events = Event.objects.all()
+    context = {'users':users, 'events':events}
+    return render(request, 'home.html', context)
